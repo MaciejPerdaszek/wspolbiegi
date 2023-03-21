39fb9a2450f817c85c65147ae3b100643e032bc3
MaciejPerdaszek <maciej.44@wp.pl>
2023-03-21 14:36:52 +0100

    fix

--- ConsoleApp1/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-using ClassLibrary1;
-Calculator calculator = new Calculator();
-int x = calculator.add(4, 6);
-Console.WriteLine(x);
