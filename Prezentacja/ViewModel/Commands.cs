fb652a96e7be043431acb7ceef2a38d0ec378811
MaciejPerdaszek <maciej.44@wp.pl>
2023-04-08 22:15:04 +0200

    Tworzenie kul w prezentacji

---
+++ Prezentacja/ViewModel/Commands.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Input;
+
+namespace Prezentacja.ViewModel
+{
+    public class Commands : ICommand
+    {
+       private readonly Action<object> onExecute;
+       private readonly Predicate<object> canExecute;
+
+        public Commands(Action<Object> execute) :
+            this(execute, null)
+        {
+        }
+
+        public Commands(Action<Object> execute, Predicate<Object> canexecute) 
+        {
+           onExecute = execute ?? throw new ArgumentNullException("execute");
+           canExecute = canexecute;
+        }
+
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return canExecute == null || canExecute(parameter);
+        }
+
+        public void Execute(object parameter) 
+        {
+            onExecute(parameter);
+        }
+
+
+
+    }
+}
--- Prezentacja/ViewModel/MainWindowViewModel.cs
+++ Prezentacja/ViewModel/MainWindowViewModel.cs
@@ -4,8 +4,11 @@ using Prezentacja.View;
 using System;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Printing;
 using System.Runtime.CompilerServices;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Shapes;
 
@@ -15,30 +18,61 @@ namespace Prezentacja.ViewModel
     {
         #region private
 
-        private readonly ModelAbstractAPI ModelApi;
+        private ModelAbstractAPI ModelApi;
 
         private ObservableCollection<IBall> Balls;
 
-
+        private int amountOfBalls;
 
         #endregion private
+       
         #region INotifyPropertyChanged
 
         public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
 
         #endregion INotifyPropertyChanged
+
         #region public API
         public MainWindowViewModel()
         {
+            MyCanvas = new Canvas();
             ModelApi = ModelAbstractAPI.CreateApi();
+            CreateBallsCommand = new Commands(CreateBallsOnBoard);
             Balls = ModelApi.getBalls();
             Balls.CollectionChanged += BallsCollectionChangedEvent;
         }
 
         public Canvas MyCanvas { get; set; }
+
+        public ICommand CreateBallsCommand { get; set; }
+
+        public string Amount
+        {
+            get
+            {
+                return Convert.ToString(amountOfBalls);
+            }
+            set
+            {
+                amountOfBalls = Convert.ToInt32(value);
+                OnPropertyChanged();
+            }
+        }
+
+        private void CreateBallsOnBoard(object obj)
+        {
+            ModelApi.CreateBalls(amountOfBalls);
+            
+        }
+
         private void BallsCollectionChangedEvent(object? sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
         {
-            foreach (IBall b in Balls) {
+            foreach (IBall b in Balls)
+            {
                 Ellipse ellipse = new Ellipse();
                 ellipse.Width = 15;
                 ellipse.Height = 15;
@@ -47,7 +81,7 @@ namespace Prezentacja.ViewModel
                 Canvas.SetLeft(ellipse, b.X);
                 Canvas.SetTop(ellipse, b.Y);
 
-                //Canvas canvas = MainWindow.FindName("canvas");
+                MyCanvas.Children.Add(ellipse);
             }
         }
 
